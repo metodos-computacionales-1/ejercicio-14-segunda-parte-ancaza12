d9f87ee673d9f732922b36211bafb55cec08921f
ancaza12 <ac.calderonz@uniandes.edu.co>
2019-11-18 11:55:41 +0000

    euler1

---
+++ 1.cpp
@@ -0,0 +1,31 @@
+//Codigo tomado de https://www.youtube.com/watch?v=UhZ1ESRR4OM
+
+#include <iostream>
+#include <cmath>
+#include <fstream>
+using namespace std;
+void integralE(float tf,float dt, float w, float m, string datos);
+
+int main(){
+    float w = 100;
+    float m=2;
+    string datos= "ej14.dat";
+    integralE(17, 0.01, w,m, datos); 
+    
+    
+    return 0;
+}
+
+void integralE(float tf,float dt, float w, float m, string datos){
+    ofstream outfile;
+    outfile.open(datos);      
+    float x=1;
+    float vE=0;
+
+    for(float t=0; t<=tf;t+=dt){
+        x-= dt*w/m*x;
+        vE= vE+ dt* vE;
+        outfile << t << x<< " " << vE<< std::endl;
+    }
+    outfile.close();
+}
