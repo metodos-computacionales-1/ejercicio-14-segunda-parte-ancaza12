7be0bfcbcf5f03f642b7bd3b3a491297ab02cc8f
Default user <ac.calderonz@uniandes.edu.co>
2019-11-18 21:02:15 +0000

    2 graficas

--- ej14.py
+++ ej14.py
@@ -2,9 +2,19 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.figure()
+plt.figure(figsize=(10,5))
+plt.subplot(2,3,1)
+plt.title("Euler")
 data = np.loadtxt('ej14.dat')
 plt.plot(data[:,0], data[:,1])
 plt.xlabel('t')
 plt.ylabel('x')
-plt.savefig('euler.png')
+
+plt.subplot(2,3,2)
+plt.title("Rk4")
+
+datos = np.loadtxt('datosrk4.dat')
+plt.plot(data[:,0], data[:,1])
+plt.xlabel('t')
+plt.ylabel('x')
+plt.savefig('ecuaciones.png')
